c2021afdb7e3d662f77544696e47f8bad43d0713
Ziyahid <ziyahid27@gmail.com>
2025-03-12 22:43:33 +0530

    login updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Records from "../../sample/src/pages/Records"
 import Navbar from "./components/Navbar"
 import FormData from "./pages/FormData"
@@ -5,15 +6,29 @@ import Login from "./pages/Login"
 import {Routes , Route} from "react-router-dom"
 
 
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+
+
 function App() {
+  const [token , setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : "")
+
+  useEffect(()=>{
+    localStorage.setItem("token" , token)
+  } , [token])
  return(
   <>
   
-   {/* <Navbar/> */}
+    {token === "" ?
+    <Login setToken= {setToken}/>:
+   <>
+   <Navbar setToken={setToken}/>
     <Routes>
-      <Route path={"/login"} element = {<Login/>}/>
+      
       <Route path={"/"} element = {<FormData/>}/>
     </Routes>
+    </>
+    }
  
   </>
  )
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { adminLogin } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+
+userRouter.post("/admin" , adminLogin)
+
+export default userRouter
